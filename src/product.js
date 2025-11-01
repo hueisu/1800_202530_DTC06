18d146cbfa831f681c6b3784506835f373e633bf
Chia Hui Su <aduxjj@gmail.com>
2025-11-01 14:33:29 -0700

    Add real product data into main page

--- src/product.js
+++ src/product.js
@@ -1,15 +1,33 @@
 import { db } from "./firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
+import $ from "jquery";
 
 async function displayProductsCards() {
   const productsRef = collection(db, "products");
+  const productContainer = $("#product-container");
 
   try {
     const querySnapshot = await getDocs(productsRef);
+    let elements = "";
     querySnapshot.forEach((doc) => {
       const product = doc.data();
-      console.log(product);
+
+      elements += `
+        <div class="border border-gray-300 rounded-md flex flex-col">
+          <div class="flex items-center justify-center grow-1">
+            <img src="${product.imageUrl}" class="" alt="${product.name}-image" />
+          </div>
+          <div class="p-3">
+            <h5 class="font-bold">${product.name}</h5>
+            <p>
+              ${product.quantity} ${product.unit} - $${product.price}
+            </p>
+          </div>
+        </div>
+      `;
     });
+
+    productContainer.html(elements);
   } catch (error) {
     console.error(error);
   }
