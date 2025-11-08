e63aa7f026c3c9b2424ad45983dded1dd7b83cd7
Matthew Tang <thematthewtang@gmail.com>
2025-11-08 15:59:21 -0800

    Change button to go to help.js instead

--- src/help.js
+++ src/help.js
@@ -1,7 +1,11 @@
-function toggleAnswer(answerID) {
-  const answer = document.getElementById(answerID);
-  answer.classList.toggle("hidden");
-  console.log(111);
-}
-
-document;
+document.addEventListener("DOMContentLoaded", function () {
+  const faqButton = document.getElementById("faq-login");
+  if (faqButton) {
+    faqButton.addEventListener("click", function () {
+      const answerID = "faq-login-answer";
+      const answer = document.getElementById(answerID);
+      answer.classList.toggle("hidden");
+      console.log(111);
+    });
+  }
+});
