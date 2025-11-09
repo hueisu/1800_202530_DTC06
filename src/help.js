54478de5d321a92b6a8bfdbddc16c89ff69b004b
Matthew Tang <thematthewtang@gmail.com>
2025-11-08 16:23:32 -0800

    Add rest of FAQ

--- src/help.js
+++ src/help.js
@@ -5,7 +5,50 @@ document.addEventListener("DOMContentLoaded", function () {
       const answerID = "faq-login-answer";
       const answer = document.getElementById(answerID);
       answer.classList.toggle("hidden");
-      console.log(111);
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const faqButton = document.getElementById("faq-shopping-add");
+  if (faqButton) {
+    faqButton.addEventListener("click", function () {
+      const answerID = "faq-shopping-add-answer";
+      const answer = document.getElementById(answerID);
+      answer.classList.toggle("hidden");
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const faqButton = document.getElementById("faq-stores");
+  if (faqButton) {
+    faqButton.addEventListener("click", function () {
+      const answerID = "faq-stores-answer";
+      const answer = document.getElementById(answerID);
+      answer.classList.toggle("hidden");
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const faqButton = document.getElementById("faq-list");
+  if (faqButton) {
+    faqButton.addEventListener("click", function () {
+      const answerID = "faq-list-answer";
+      const answer = document.getElementById(answerID);
+      answer.classList.toggle("hidden");
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const faqButton = document.getElementById("faq-data");
+  if (faqButton) {
+    faqButton.addEventListener("click", function () {
+      const answerID = "faq-data-answer";
+      const answer = document.getElementById(answerID);
+      answer.classList.toggle("hidden");
     });
   }
 });
